a677e412b8691001c76cf264f8616a19525fefff
filow <Sferfewe99sfw@mail.ru>
2023-06-22 19:54:26 +0300

    wip 20%

---
+++ main.js
@@ -0,0 +1,6 @@
+const age = prompt('Ваш возраст')
+const elName  = document.getElementById('userName')
+if ( age < 9) { location.replace('https://ya.ru') }
+
+if ( age > 99) { alert ('Слишком взрослый')}
+elName.innerText = name;
